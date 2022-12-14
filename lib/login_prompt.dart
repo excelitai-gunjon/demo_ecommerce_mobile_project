14b98fa11b1a12cf9cfe1d86ca5edcd7f56aceab
nazmus <nazmus.s.ashrafi@gmail.com>
2022-12-14 12:58:44 +0600

    new update

--- lib/login_prompt.dart
+++ lib/login_prompt.dart
@@ -9,86 +9,91 @@ class LoginPrompt extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.arrow_back_ios_new_outlined, color: Colors.black,),
-        title: const Text("Cart",
+        leading: Icon(Icons.arrow_back_ios_new_outlined, color: Colors.white,),
+        title: const Text("",
           style: TextStyle(
-              color: Colors.black
+              color: Colors.white
           ),),
-        backgroundColor: Colors.white70,
+        backgroundColor: Colors.red.shade400,
       ),
       body: Container(
+
         decoration: BoxDecoration(color: Colors.brown.shade50),
         child: Center(child:
         Container(
-
+          height: AppDimension.padding(figPadding: 400, context: context),
+          width: AppDimension.padding(figPadding: 330, context: context),
           decoration: BoxDecoration(
-            color: Colors.black12,
-            borderRadius: BorderRadius.circular(AppDimension.padding(figPadding: 6, context: context)),
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(AppDimension.padding(figPadding: 8, context: context)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black45,
+                blurStyle: BlurStyle.normal,
+                spreadRadius: -15,
+                blurRadius: 20,
+                offset: Offset(-5.0, 20.0),
+              ),
+
+            ]
           ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-
-              // Icon
-              Container(
-                padding: EdgeInsets.all(AppDimension.padding(figPadding: 12, context: context)),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(100),
-                  border: Border.all(width: AppDimension.padding(figPadding: 2, context: context),
-                      color: Colors.white),
-                ),
-
-                child: Icon(
-                  Icons.shopping_cart_outlined,
-                  color: Colors.black45,
-                  size: AppDimension.padding(figPadding: 58, context: context),
-
-                ),
-              ),
-
-              // Padding
-              SizedBox(height: AppDimension.padding(figPadding: 30, context: context),),
-
-              // Text Major
-              Text.rich(
-                TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'Your Cart is Empty',
-                      style: TextStyle(
-                          fontSize: AppDimension.width(figmaWidth: 14, context: context),
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black38),
-                    ),
-                  ],
-                ),
-              ),
 
               // Padding
               SizedBox(height: AppDimension.padding(figPadding: 10, context: context),),
 
-              // Text Minor
-              Text.rich(
-                TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'Looks like you have not added anything, try adding to your cart',
-                      style: TextStyle(
-                          fontSize: AppDimension.width(figmaWidth: 8, context: context),
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black38),
-                    ),
-                  ],
+              // Text Major
+              Padding(
+                padding: const EdgeInsets.all(28.0),
+                child: Text('You must be logged in to access this functionality.',
+                  maxLines: 2,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                        fontSize: AppDimension.width(figmaWidth: 14, context: context),
+                        fontWeight: FontWeight.normal,
+                        color: Colors.red.shade400),
+                  ),
                 ),
-              ),
+
 
               // Padding
-              SizedBox(height: AppDimension.padding(figPadding: 50, context: context),),
+              SizedBox(height: AppDimension.padding(figPadding: 10, context: context),),
+
+              // Picture
+              // Container(
+              //   padding: EdgeInsets.all(AppDimension.padding(figPadding: 12, context: context)),
+              //   decoration: BoxDecoration(
+              //     color: Colors.red,
+              //     borderRadius: BorderRadius.circular(100),
+              //     border: Border.all(width: AppDimension.padding(figPadding: 2, context: context),
+              //         color: Colors.white),
+              //   ),
+              //
+              //   child: Icon(
+              //     Icons.shopping_cart_outlined,
+              //     color: Colors.black45,
+              //     size: AppDimension.padding(figPadding: 58, context: context),
+              //
+              //   ),
+              // ),
+              Image.asset(
+                "assets/images/food.png",
+                width: AppDimension.padding(figPadding: 150,context: context),
+                height: AppDimension.padding(figPadding: 150,context: context),
+              ),
+
+
+              // Padding
+              SizedBox(height: AppDimension.padding(figPadding: 40, context: context),),
 
               // Button
-              CustomButton(buttonText: "START SHOPPING"),
+              CustomButton(buttonText: "LOGIN / SIGN UP"),
+
+              // Padding
+              SizedBox(height: AppDimension.padding(figPadding: 30, context: context),),
 
             ],
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:empty_cart_page/checkout_page/Checkout.dart';
 import 'package:empty_cart_page/empty_cart.dart';
 import 'package:empty_cart_page/login_prompt.dart';
 import 'package:flutter/material.dart';
@@ -14,12 +15,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Contribution to ecommerce app',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: const LoginPrompt(),
+      // const LoginPrompt()
+      // const Checkout(),
       // const EmptyCart(),
     );
   }
