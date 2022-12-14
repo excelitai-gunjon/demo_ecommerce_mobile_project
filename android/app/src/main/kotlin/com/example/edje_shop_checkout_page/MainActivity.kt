f93bb681acfd07682cc38c936be3faa969daf4d7
nazmus <nazmus.s.ashrafi@gmail.com>
2022-12-14 09:59:22 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/edje_shop_checkout_page/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.edje_shop_checkout_page
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Checkout.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:edje_shop_checkout_page/DeliveryDetails.dart';
+
+class Checkout extends StatelessWidget {
+  const Checkout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back_ios_new_outlined, color: Colors.black,),
+        title: const Text("Checkout",
+        style: TextStyle(
+          color: Colors.black
+        ),),
+        backgroundColor: Colors.white70,
+
+      ),
+      body: ListView(
+        children: [
+            DeliveryDetails(),
+
+        ],
+      ),
+    );
+  }
+}
